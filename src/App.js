3dea484dc7361d7998b67fbc62433c70740bf3ed
vitaretnama <nurvitaretnama@gmail.com>
2023-01-30 13:18:17 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import './App.css';
+import {Hasil, ListCategories, Menus, NavbarComponent} from './components';
+import { API_URL } from './utils/constant';
+import axios from "axios"
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       menus: [], // menu ini isinya akan kosong dan diisikan dengan product
+       kategoriYangDipilih: "Makanan" //default dari kategori yang dipilih yaitu makanan, nah nanti akan diklik sesuai dengan user yang memilih
+    }
+  }
+  componentDidMount() {
+    axios
+      .get(API_URL+"products?category.nama="+this.state.kategoriYangDipilih)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // Membuat arrow function berdasarkan kategori yang dipilih
+
+  changeCategory = (value) => {
+    this.setState ({ // function ini berfungsi untuk mengubah kategori yang dipilih berdasarkan user
+      kategoriYangDipilih: value, // nah di dalam setState, maka kategori yang dipilih dimasukkan dan diganti dengan props value
+      menus: [] // pada function ini juga menusnya diganti dengan array kosong kembali
+    })
+
+    axios
+    .get(API_URL+"products?category.nama="+value)
+    .then(res => {
+      const menus = res.data;
+      this.setState({ menus });
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+  
+  render() {
+    const {menus, kategoriYangDipilih} = this.state
+    return (
+      <div className="App">
+      <NavbarComponent/>
+      <div className='mt-4'>
+        <Container>
+          <Row>
+            <ListCategories changeCategory={this.changeCategory} kategoriYangDipilih={kategoriYangDipilih} />
+            <Col>
+              <h4><strong>Daftar Produk</strong></h4>
+              <hr />
+              <Row>
+                {menus && menus.map((menu) => (
+                  <Menus 
+                    key={menu.id}
+                    menu={menu}
+                  />
+                ))}
+              </Row>
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>  
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+
+
